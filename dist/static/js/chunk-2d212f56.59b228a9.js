6d9f8326d973c274ff0604fd37370536a381d179
unknown <923361719@qq.com>
2020-06-01 10:17:41 +0800

    add dist

---
+++ dist/static/js/chunk-2d212f56.59b228a9.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d212f56"],{ab43:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("发通知（给全部商家/全部用户，）  通知列表")])},c=[],l=t("2877"),a={},r=Object(l["a"])(a,u,c,!1,null,null,null);e["default"]=r.exports}}]);
